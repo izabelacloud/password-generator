99ed4e6f51476c5f3068801d22113734e8354dea
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-03-29 22:50:48 -0700

    committing changes of the development

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,108 @@
-// Assignment code here
+//start of the generatePassword function
+
+var generatePassword = function() {
+
+  var characterCount = window.prompt("Enter the count of characters betweek 8 and 128.");
+  console.log(characterCount);
+
+  if (characterCount >= 8 && characterCount <= 128) {
+
+  var capitalLetterCheck = window.confirm("Would you like to include Capital letters?");
+  console.log(capitalLetterCheck);
+
+  var lowerCaseLetterCheck = window.confirm("Would you like to include Lower Case letters");
+  console.log(lowerCaseLetterCheck);
+
+  var specialCharacterCheck = window.confirm("Would you like to include Special characters?");
+  console.log(specialCharacterCheck);
+
+  var numberCheck = window.confirm("Would you like to include Numbers?");
+  console.log(numberCheck);
+    
+  }
+  else {
+    window.alert("You did not select the correct number of characters. Try again!")
+    generatePassword();
+    //this needs to be further tuned also
+  }
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//constants
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const capitalLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","R","S","T","U","V","X","Y","Z"];
+const lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","x","y","z"];
+const specialCharacters = ["&","%","?","<",">","@","$","*","#","!"];
+const numbers = ["1","2","3","4","5","6","7","8","9","0"];
 
-  passwordText.value = password;
+//overall combinedArray definition
+
+const combinedArray = capitalLetters.concat(lowercaseLetters).concat(specialCharacters).concat(numbers);
+//test
+console.log(combinedArray);
+
+var password = "";
+
+
+//Need to tune this section more to figure out how to do all the combinations of user selections --> will be handled in future releases
+
+if(capitalLetterCheck && lowerCaseLetterCheck && specialCharacterCheck && numberCheck ) {
+  const combinedAllArrays = capitalLetters.concat(lowercaseLetters).concat(specialCharacters).concat(numbers);
+  console.log(combinedAllArrays);
+
+  for(var i = 0; i < characterCount; i++) {
+    password = password + combinedAllArrays[Math.floor(Math.random() * Math.floor(combinedAllArrays.length - 1))];
+    console.log(password);
+  }
+}
+
+if(!capitalLetterCheck && lowerCaseLetterCheck && specialCharacterCheck && numberCheck ) {
+  const upperCaseLettersNotIncluded = lowercaseLetters.concat(specialCharacters).concat(numbers);
+  console.log(upperCaseLettersNotIncluded);
+
+  for(var i = 0; i < characterCount; i++) {
+    password = password + upperCaseLettersNotIncluded[Math.floor(Math.random() * Math.floor(upperCaseLettersNotIncluded.length - 1))];
+    console.log(password);
+  }
+}
+
+if(capitalLetterCheck && !lowerCaseLetterCheck && specialCharacterCheck && numberCheck ) {
+  const lowerCaseLettersNotIncluded = capitalLetters.concat(specialCharacters).concat(numbers);
+  console.log(lowerCaseLettersNotIncluded);
+
+  for(var i = 0; i < characterCount; i++) {
+    password = password + lowerCaseLettersNotIncluded[Math.floor(Math.random() * Math.floor(lowerCaseLettersNotIncluded.length - 1))];
+    console.log(password);
+  }
+}
+
+if(capitalLetterCheck && lowerCaseLetterCheck && !specialCharacterCheck && numberCheck ) {
+  const specialCharacterNotIncluded = capitalLetters.concat(lowercaseLetters).concat(numbers);
+  console.log(specialCharacterNotIncluded);
+
+  for(var i = 0; i < characterCount; i++) {
+    password = password + specialCharacterNotIncluded[Math.floor(Math.random() * Math.floor(specialCharacterNotIncluded.length - 1))];
+    console.log(password);
+  }
+}
+
+if(capitalLetterCheck && lowerCaseLetterCheck && specialCharacterCheck && !numberCheck ) {
+  const numbersNotIncluded = capitalLetters.concat(lowercaseLetters).concat(specialCharacters);
+  console.log(numbersNotIncluded);
+
+  for(var i = 0; i < characterCount; i++) {
+    password = password + numbersNotIncluded[Math.floor(Math.random() * Math.floor(numbersNotIncluded.length - 1))];
+    console.log(password);
+  }
+}
+
+
+  document.getElementById("password").value = password;
+  var generateBtn = document.querySelector("#generate");
+  // generateBtn.addEventListener("click", writePassword);
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+    
